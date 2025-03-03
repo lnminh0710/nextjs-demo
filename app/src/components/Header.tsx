d44525526fb8a573ff45e81bde345604f770b95d
lnminh0710 <39968891+lnminh0710@users.noreply.github.com>
2025-03-03 10:29:57 +0700

    Update Header.tsx

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -11,11 +11,11 @@ export const HeaderComponent = () => {
 
     return (
         <div className="h-15 flex items-center justify-between p-3 border-b-1 bg-white border-[#e8eaee]">
-            <div className="flex items-center gap-1 text-red-700">
+            <div className="flex items-center gap-1 text-blue-700">
                 <Link href={"demo-1"}>Demo 1</Link>
                 <Link href={"demo-2"}>Demo 2</Link>
             </div>
-            <div className="text-amber-400 ml-4">Counter: {counter}</div>
+            <div className="text-red-600 ml-4">Counter: {counter}</div>
             <div>
                 {data?.firstName} {data?.lastName}
             </div>
